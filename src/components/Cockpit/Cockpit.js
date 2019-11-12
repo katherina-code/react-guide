087d099920bff92c0c816085e6cad58c078263ec
katherina <katherina220495@gmail.com>
2019-11-12 18:16:37 +0300

    lifecycle and hooks

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -1,7 +1,25 @@
-import React, { Fragment } from 'react';
+import React, { useEffect } from 'react';
 import classes from './Cockpit.css'
 
 const cockpit = (props) => {
+    useEffect(() => {
+        console.log('[Cockpit.js] useEffect');
+        // Http request...
+        setTimeout(() => {
+            alert('Save data to cloud!')
+        }, 1000);
+        return () => {
+            console.log('[Cockpit.js] cleanup work in useEffect');
+        };
+    }, []);
+
+    useEffect(() => {
+        console.log('[Cockpit.js] 2nd useEffect');
+        return () => {
+            console.log('[Cockpit.js] cleanup work in 2nd useEffect');
+        };
+    });
+
     const assignedClasses = [];
     let btnClass = '';
 
--- src/containers/App.js
+++ src/containers/App.js
@@ -4,13 +4,41 @@ import Persons from '../components/Persons/Persons'
 import Cockpit from '../components/Cockpit/Cockpit';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    console.log('[App.js] constructor')
+  }
+
   state = {
     persons: [
       { id: 'frfr', name: 'Max', age: '22' },
       { id: 'frfr1', name: 'Kate', age: '23' },
       { id: 'frfr2', name: 'Elena', age: '24' },
     ],
-    showPersons: false
+    showPersons: false,
+    showCockpit: true
+  }
+
+  static getDerivedStateFromProps(props, state) {
+    console.log('[App.js] getDerivedStateFromProps', props);
+    return state;
+  }
+
+  // componentWillMount() {
+  //   console.log('[App.js] componentWillMount')
+  // }
+
+  componentDidMount() {
+    console.log('[App.js] componentDidMount');
+  }
+
+  componentDidUpdate() {
+    console.log('[App.js] componentDidUpdate');
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    console.log('[App.js] shouldComponentUpdate');
+    return true;
   }
 
   nameChangedHandler = (event, id) => {
@@ -45,6 +73,7 @@ class App extends Component {
   };
 
   render() {
+    console.log('[App.js], render')
     let persons = null;
 
     if (this.state.showPersons) {
@@ -56,12 +85,17 @@ class App extends Component {
 
     return (
       <div className={classes.App}>
-        <Cockpit
-          title={this.props.appTitle}
-          persons={this.state.persons}
-          showPersons={this.state.showPersons}
-          clicked={this.togglePersonsHandler}
-        />
+        <button onClick={() => { this.setState({ showCockpit: false }) }}>Remove Cockpit</button>
+        {
+          this.state.showCockpit ?
+            <Cockpit
+              title={this.props.appTitle}
+              persons={this.state.persons}
+              showPersons={this.state.showPersons}
+              clicked={this.togglePersonsHandler}
+            />
+            : null
+        }
         {persons}
       </div>
     );
