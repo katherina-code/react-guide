f58ad96f3bb60f8fd3855ee3a5bcf2f9b80e2569
katherina <katherina220495@gmail.com>
2019-11-27 15:49:26 +0300

    27.11.19

--- src/components/Persons/Persons.js
+++ src/components/Persons/Persons.js
@@ -15,7 +15,12 @@ class Persons extends Component {
     shouldComponentUpdate(nextProps, nextState) {
         console.log('[Persons.js] shouldComponentUpdate');
         console.log(nextProps);
-        return true;
+        if (nextProps.persons !== this.props.persons) {
+            return true;
+        } else {
+            return false;
+        }
+        // return true;
     }
 
     getSnapshotBeforeUpdate(prevProps, prevState) {
@@ -24,7 +29,7 @@ class Persons extends Component {
     }
 
     // componentWillUpdate() {
-        
+
     // }
 
     componentDidUpdate(prevProps, prevState, snapshot) {
